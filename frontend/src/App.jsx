956543a6f9bd8e1b91ec6d38b1a10d567f412c13
Yash-D-Rawat <yash.rawat2117@gmail.com>
2025-03-08 19:42:57 +0530

    collaboration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LogIn from "./pages/Login";
+import WhisperSpeechToText from "./pages/WhisperSpeechToText ";
 import Footer from "./components/Footer";
 import { Navbar } from "./components/Navbar";
+import TextImage from "./pages/TextImage";
 
 function App() {
   return (
@@ -11,7 +13,8 @@ function App() {
         <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/Login" element={<LogIn />} /> 
-        
+        <Route path="/WhisperSpeech" element={<WhisperSpeechToText/>} /> 
+        <Route path="/TextImage" element={<TextImage/>} />
         </Routes>
          
       </Router>
